8b8e8c3a6602d499544de34cb06f8f9ae8246220
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-05-06 10:35:54 +0200

    Restful_Web_Services: creating user, user controller, user dao

--- resful-web-services/src/main/java/com/in28minutes/rest/webservices/resfulwebservices/helloworld/HelloWorldBean.java
+++ resful-web-services/src/main/java/com/in28minutes/rest/webservices/resfulwebservices/helloworld/HelloWorldBean.java
@@ -1,4 +1,4 @@
-package com.in28minutes.rest.webservices.resfulwebservices;
+package com.in28minutes.rest.webservices.resfulwebservices.helloworld;
 
 public class HelloWorldBean {
 	
--- resful-web-services/src/main/java/com/in28minutes/rest/webservices/resfulwebservices/helloworld/HelloWorldController.java
+++ resful-web-services/src/main/java/com/in28minutes/rest/webservices/resfulwebservices/helloworld/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.in28minutes.rest.webservices.resfulwebservices;
+package com.in28minutes.rest.webservices.resfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;

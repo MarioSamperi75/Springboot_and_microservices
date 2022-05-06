447cabaf73e8f95765cd920920c804f997a01f5c
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-05-06 14:40:01 +0200

    Restful_Web_Services: returning  201 CREATED and the path as response

--- resful-web-services/src/main/java/com/in28minutes/rest/webservices/resfulwebservices/user/UserResource.java
+++ resful-web-services/src/main/java/com/in28minutes/rest/webservices/resfulwebservices/user/UserResource.java
@@ -1,14 +1,17 @@
 package com.in28minutes.rest.webservices.resfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -27,8 +30,19 @@ public class UserResource {
 	}
 	
 	@PostMapping("/users")
-	public void retrieveUser (@RequestBody User user) {
+	public ResponseEntity<Object> retrieveUser (@RequestBody User user) {
 		User savedUser = service.saveUser(user);
+		
+		//adding CREATED and the path as response
+		URI location = ServletUriComponentsBuilder
+			.fromCurrentRequest()
+			.path("/{id}")
+			.buildAndExpand(savedUser.getId())
+			.toUri();
+		
+		return ResponseEntity.created(location).build();
+		
+		
 	}
 	
 

082b23894a67b0ec8c906fa488ea90e1c697d5e3
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-05-09 12:09:20 +0200

    Restful_Web_Services: Implementing DELETE Method to forImplementing DELETE Method to delete a User

--- resful-web-services/src/main/java/com/in28minutes/rest/webservices/resfulwebservices/user/UserDaoService.java
+++ resful-web-services/src/main/java/com/in28minutes/rest/webservices/resfulwebservices/user/UserDaoService.java
@@ -2,6 +2,7 @@ package com.in28minutes.rest.webservices.resfulwebservices.user;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
@@ -49,4 +50,19 @@ public class UserDaoService {
 		return null;
 	}
 	
- }
+	//Trying to remove items using a for loop or a for-each loop would not work correctly 
+	//because the collection is changing size at the same time that the code is trying to loop.
+	public User deleteById(int id) {
+		
+		Iterator<User> iterator = users.iterator();
+		while (iterator.hasNext()) {
+			User user = iterator.next();
+			if (user.getId() == id) {
+			iterator.remove();
+			return user;
+			}
+		}
+		return null;
+	}
+
+}

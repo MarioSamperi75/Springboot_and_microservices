7e47eb5689417088358b5e1856b43b2102629b4e
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-05-09 15:40:28 +0200

    Restful_Web_Services: internationalization - MessageSource, Local, properties

--- resful-web-services/src/main/java/com/in28minutes/rest/webservices/resfulwebservices/helloworld/HelloWorldController.java
+++ resful-web-services/src/main/java/com/in28minutes/rest/webservices/resfulwebservices/helloworld/HelloWorldController.java
@@ -1,15 +1,39 @@
 package com.in28minutes.rest.webservices.resfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+//internationalization
+// create the messageSource object
+// create the properties file with that exact syntax
+// create the local object as parameter (request header) 
+// OR pick it up from the local context holder 
 
 @RestController
 public class HelloWorldController {
+
+	@Autowired
+	private MessageSource messageSource;
 	
 	@GetMapping("/hello-world")
 	public String helloWorld() {
 		return "Hello World";
+	}
+	
+	@GetMapping("/hello-world-internationalized")
+	public String helloWorldInternationalized(
+		//	@RequestHeader(name="Accept-Language", required = false ) Locale locale
+			) {
+		
+		return messageSource.getMessage("good.morning.message", null, "Default Message", LocaleContextHolder.getLocale());
+		//return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
 	}
 	
 	@GetMapping("/hello-world-bean")

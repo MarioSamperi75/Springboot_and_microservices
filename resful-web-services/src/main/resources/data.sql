ba2715afbb82425253dfe8c276e9c2d8587b4de1
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-05-10 15:17:32 +0200

    Restful_Web_Services: JPA: User Entity, some data and properties file

---
+++ resful-web-services/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into user values(1, sysdate(),'Mario');
+insert into user values(2, sysdate(),'Ellen');
+insert into user values(3, sysdate(),'Sofia');
+insert into user values(4, sysdate(),'Alexander');

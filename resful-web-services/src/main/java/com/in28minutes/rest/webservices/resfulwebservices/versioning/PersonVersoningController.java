8afd42beabbc4e695c2ae0e193da12215fbcb7a1
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-05-10 11:42:32 +0200

    Restful_Web_Services: versioning - params solution

--- resful-web-services/src/main/java/com/in28minutes/rest/webservices/resfulwebservices/versioning/PersonVersoningController.java
+++ resful-web-services/src/main/java/com/in28minutes/rest/webservices/resfulwebservices/versioning/PersonVersoningController.java
@@ -3,11 +3,11 @@ package com.in28minutes.rest.webservices.resfulwebservices.versioning;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// basic solution for versioning: use differents URLs
+
 
 @RestController
 public class PersonVersoningController {
-	
+	// basic solution for versioning: use differents URLs
 	@GetMapping("/v1/person")
 	public PersonV1 getPersonV1 () {
 		return new PersonV1("Mario Samperi");
@@ -15,6 +15,18 @@ public class PersonVersoningController {
 	
 	@GetMapping("/v2/person")
 	public PersonV2 getPersonV2 () {
+		return new PersonV2(new Name("Mario", "Samperi"));
+	}
+	
+	// basic solution for versioning: use differents params
+	// .../person/param?version=1
+	@GetMapping(value="/person/param", params="version=1")
+	public PersonV1 getPersonV1Param () {
+		return new PersonV1("Mario Samperi");
+	}
+	
+	@GetMapping(value="/person/param", params="version=2")
+	public PersonV2 getPersonV2Param () {
 		return new PersonV2(new Name("Mario", "Samperi"));
 	}
 

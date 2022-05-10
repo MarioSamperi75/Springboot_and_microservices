ae8584a3d5461474c644487975d9908535ff2b56
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-05-10 11:51:04 +0200

    Restful_Web_Services: versioning - header solution

--- resful-web-services/src/main/java/com/in28minutes/rest/webservices/resfulwebservices/versioning/PersonVersoningController.java
+++ resful-web-services/src/main/java/com/in28minutes/rest/webservices/resfulwebservices/versioning/PersonVersoningController.java
@@ -18,7 +18,7 @@ public class PersonVersoningController {
 		return new PersonV2(new Name("Mario", "Samperi"));
 	}
 	
-	// basic solution for versioning: use differents params
+	// basic solution for versioning: use different params
 	// .../person/param?version=1
 	@GetMapping(value="/person/param", params="version=1")
 	public PersonV1 getPersonV1Param () {
@@ -27,6 +27,18 @@ public class PersonVersoningController {
 	
 	@GetMapping(value="/person/param", params="version=2")
 	public PersonV2 getPersonV2Param () {
+		return new PersonV2(new Name("Mario", "Samperi"));
+	}
+	
+	// basic solution for versioning: use different headers
+	// you should add the header in your request
+	@GetMapping(value="/person/header", headers="X-API-VERSION=1")
+	public PersonV1 getPersonV1Headers () {
+		return new PersonV1("Mario Samperi");
+	}
+	
+	@GetMapping(value="/person/header", headers="X-API-VERSION=2")
+	public PersonV2 getPersonV2Headers () {
 		return new PersonV2(new Name("Mario", "Samperi"));
 	}
 

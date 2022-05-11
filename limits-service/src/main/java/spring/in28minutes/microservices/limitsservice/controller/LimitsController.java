d9a27e5c313f844b0b8793db7cf0662bbe597547
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-05-11 14:20:35 +0200

    Microsevices_SpringCloud:  get properties from properties file - configuration

--- limits-service/src/main/java/spring/in28minutes/microservices/limitsservice/controller/LimitsController.java
+++ limits-service/src/main/java/spring/in28minutes/microservices/limitsservice/controller/LimitsController.java
@@ -1,17 +1,24 @@
 package spring.in28minutes.microservices.limitsservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import spring.in28minutes.microservices.limitsservice.bean.Limits;
+import spring.in28minutes.microservices.limitsservice.configuration.Configuration;
 
 @RestController
 public class LimitsController {
+	
+	@Autowired
+	Configuration configuration;
 
 	@GetMapping("/limits")
 	public Limits retrieveLimits () {
 		
-		return new Limits(1,1000);
+		return new Limits(configuration.getMinimum(), configuration.getMaximum());
+		
+		//return new Limits(1,1000);
 		
 	}
 }

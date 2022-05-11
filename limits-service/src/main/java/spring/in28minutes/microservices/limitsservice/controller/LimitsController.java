de2fcc878a46f01d9bca544dbd8e2f535e4c276c
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-05-11 14:05:57 +0200

    Microsevices_SpringCloud: create the limits bean and the limits controller

---
+++ limits-service/src/main/java/spring/in28minutes/microservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,17 @@
+package spring.in28minutes.microservices.limitsservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import spring.in28minutes.microservices.limitsservice.bean.Limits;
+
+@RestController
+public class LimitsController {
+
+	@GetMapping("/limits")
+	public Limits retrieveLimits () {
+		
+		return new Limits(1,1000);
+		
+	}
+}

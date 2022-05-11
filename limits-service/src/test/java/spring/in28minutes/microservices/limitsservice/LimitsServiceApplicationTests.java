913612a5592c09a3944e50c60b57e6acd43aae60
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-05-11 13:44:58 +0200

    starting project and properties

---
+++ limits-service/src/test/java/spring/in28minutes/microservices/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.in28minutes.microservices.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

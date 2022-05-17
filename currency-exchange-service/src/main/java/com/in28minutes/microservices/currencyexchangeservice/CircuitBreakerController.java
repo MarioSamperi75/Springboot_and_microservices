04ef8633d963967116df870699cff1b04e9a8087
MarioSamperi75 <55873865+MarioSamperi75@users.noreply.github.com>
2022-05-17 14:57:02 +0200

    Microservices_SpringCloud: Resilience4j - @Bulkheader

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -6,37 +6,45 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 
 @RestController
 public class CircuitBreakerController {
 	Logger logger = org.slf4j.LoggerFactory.getLogger(CircuitBreakerController.class);
-	
-	
-	//@Retry(name="default") 	// default is 3 times
-	
-	// you can configure sample-api in the properties a provide a fallback method
-	//@Retry(name="sample-api", fallbackMethod = "hardCodedResponse") 
-	
+
+	// @Retry(name="default") // default is 3 times
+
+	// you can configure retry in the properties a provide a fallback method
+	// @Retry(name="sample-api", fallbackMethod = "hardCodedResponse")
+
 	// CircuitBreaker stops sending request continuously if the server is down
-	// it keeps checking the server sometimes and "open" it again when/if the problem is solved
-	//@CircuitBreaker(name="default", fallbackMethod = "hardCodedResponse")
-	
-	//RateLimiter - e.g.  1000 request allowed in 10s
-	@RateLimiter(name = "default")
+	// it keeps checking the server sometimes and "open" it again when/if the
+	// problem is solved
+	// you can configure circuitbreaker in the properties
+	// @CircuitBreaker(name="default", fallbackMethod = "hardCodedResponse")
+
+	// RateLimiter - e.g. 1000 request allowed in 10s
+	// you can configure RateLimiter in the properties
+
+	// @RateLimiter(name = "default")
+
+	// Bulkhead: how many bulkhead requests are allowed
+	// you can configure retry in the properties
+	@Bulkhead(name = "sample-api")
 	@GetMapping("/sample-api")
 	public String sampleApi() {
-		
+
 		logger.info("Sample API call received");
-		//we just call a not existing URL to create an error
+		// we just call a not existing URL to create an error
 //		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/not-existing-url", String.class);
 //		
 //		return forEntity.getBody();
 		return "Sample API";
-		
+
 	}
-	
-	public String hardCodedResponse (Exception ex) {
+
+	public String hardCodedResponse(Exception ex) {
 		return "Fallback-Response";
 	}
 
